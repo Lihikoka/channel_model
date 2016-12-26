ff17ea201fe0dd6f7894115f7bceb15b723cba57
Lihikoka <dainelcofa@gmail.com>
2016-12-26 22:26:49 +0800

    plot pdf

--- CIR_practice/main.m
+++ CIR_practice/main.m
@@ -1,7 +1,7 @@
 clc
 clear
 
-frequency = '28_GHz';
+frequency = '73_GHz';
 scenario = 'outdoor';
 N=100;
 string = ['load Results_',frequency,'_',scenario,'_',num2str(N),'_CIRs'];
@@ -21,9 +21,11 @@ AOAStdev = std(allAOA);
 %%%%%%%%%% plot AoA distribution
 AOARange = 0:0.01:360;
 AOAGraph = zeros(1, 36001);
+AOApdf = zeros(1, 36001);
 
 for i = 1:length(allAOA)
     pos          = find(AOARange>=allAOA(i)); 
+%     AOAIndex = round(AOARange(pos(1)));
     AOAIndex = ((AOARange(pos(1))-allAOA(i))*(pos(1)-1) + ... % interpolation
                 (allAOA(i)-AOARange(pos(1)-1))*(pos(1))) / ...
                 (AOARange(pos(1))-AOARange(pos(1)-1));
@@ -31,8 +33,18 @@ for i = 1:length(allAOA)
     AOAGraph(AOAIndex) = AOAGraph(AOAIndex) + 1;
 end
 
+sum = 0;
+for i = 1:length(AOARange)
+    sum = sum + AOAGraph(i);
+    if ~mod(AOARange(i), 1)
+        AOApdf(i) = sum/length(allAOA);
+        sum=0;
+    end
+end
+
+
 figure(1)
-plot(AOARange, AOAGraph)
+plot(AOARange, AOApdf)
 xlabel('AOA(deg)');
 ylabel('number');
 
